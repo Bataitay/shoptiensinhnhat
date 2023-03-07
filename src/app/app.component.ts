cf8025c608bffd705e24d4cd36952323b86c9eaa
Bataitay <duongmg99@gmail.com>
2023-03-07 07:52:36 +0700

    check data empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, ViewChild } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { AppService } from './app.service';
 import { ModalComponent } from './modal/modal.component';
+import Swal from 'sweetalert2';
 declare var window: any;
 
 @Component({
@@ -17,10 +18,11 @@ export class AppComponent {
   faceBook = '';
   phone = '';
   zalo = '';
-  quantity:any;
+  quantity: any;
   formModal: any;
   serachBase !: FormGroup;
   serachCouple !: FormGroup;
+  message: string = '';
   constructor(
     private fb: FormBuilder,
     private appService: AppService,
@@ -42,7 +44,6 @@ export class AppComponent {
     this.formModal = new window.bootstrap.Modal(
       document.getElementById('check-order')
     )
-    this.getOrder = JSON.parse(localStorage['data_order']);
   }
 
   handdleSearch() {
@@ -50,9 +51,20 @@ export class AppComponent {
     let month = this.serachBase.value.month
     let year = this.serachBase.value.year
     let search = day + '/' + month + '/' + year;
-    this.appService.searchBase(search).then((res: any) => {
-      this.data = res.data.list;
-    })
+    if (day || month || year) {
+      this.appService.searchBase(search).then((res: any) => {
+        this.data = res.data.list;
+        if (res.data.list.length == 0) {
+          this.message = 'Số này hiện không có, bạn liên hệ Fanpage hoặc Zalo để được tư vấn thêm';
+        }
+      })
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Có lỗi xảy ra!',
+        text: 'Vui lòng điền ngày tháng năm sinh',
+      })
+    }
   }
   handdleSearchCouple() {
     let year1 = this.serachCouple.value.year1
@@ -61,21 +73,34 @@ export class AppComponent {
       year1: year1,
       year2: year2,
     }
-    this.appService.searchCouple(value).then((res: any) => {
-      this.data = res.data.list;
-    })
+    if (value) {
+      this.appService.searchCouple(value).then((res: any) => {
+        this.data = res.data.list;
+        if (res.data.list.length == 0) {
+          this.message = 'Số này hiện không có, bạn liên hệ Fanpage hoặc Zalo để được tư vấn thêm';
+        }
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Có lỗi xảy ra!',
+        text: 'Vui lòng điền ngày tháng năm sinh',
+      })
+    }
   }
   open() {
     this.formModal.show()
   }
   addToCart(item: any) {
     if (item) {
-      if (localStorage['data_order']?.length == 2) {
-        this.listOrder = [];
-        this.getOrder = [];
+      let checkData = localStorage['data_order'];
+
+      if (checkData) {
+        this.getOrder = JSON.parse(checkData);
       }
-      this.listOrder.push(item);
-      localStorage.setItem('data_order', JSON.stringify(this.listOrder));
+
+      this.getOrder.push(item);
+      localStorage.setItem('data_order', JSON.stringify(this.getOrder));
       this.getOrder = JSON.parse(localStorage['data_order']);
       this.quantity = this.getOrder.length;
     }
